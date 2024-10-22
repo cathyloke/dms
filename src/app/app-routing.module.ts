fa65fe72e9699f175ddf600449e369e08fda635f
cathyloke <catloke963@gmail.com>
2024-10-22 20:49:44 +0800

    Created project folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
+// import { AbcComponent } from './components/abc/abc.component';
+// import { DefComponent } from './components/def/def.component';
+
+// export const routes: Routes = [
+//   { path: '', redirectTo: 'abc', pathMatch: 'full' },
+//   { path: 'abc', component: AbcComponent },
+//   { path: 'def', component: DefComponent },
+// ];
+
+
+// ng serve -o (auto open browser)
+// ng g c login
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
